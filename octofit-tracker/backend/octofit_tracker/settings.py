78764a9d32d52c685da771a677e06a7dfa1aadcb
Adhil Muhammed <67159195+adhilmuhammed03@users.noreply.github.com>
2025-04-08 19:27:25 +0000

    Updated views and settings to support codespace URL and localhost for API endpoints

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ht71wfmonnhm3bw-ndm!di%*p+*2+qur54iim3ctcbe@x13h#u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev"]
 
 
 # Application definition
